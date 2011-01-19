48eefbac2eefcb0af32374b0318c9a6e79b9b507
AoD314 <morozov.andrey.vmk@gmail.com>
2011-01-20 02:45:39 +0300

    Начал писать тест на класс Ray

---
+++ rt2/ray.cpp
@@ -0,0 +1,11 @@
+
+#include "ray.hpp"
+
+namespace rt2
+{
+	Ray::Ray(mlib::vec4 pos, mlib::vec4 dir)
+	{
+
+	}
+}
+
---
+++ rt2/ray.hpp
@@ -0,0 +1,21 @@
+
+
+#ifndef __RAY_HPP__
+#define __RAY_HPP__
+
+#include "mlib/fixed_vector.hpp"
+
+namespace rt2
+{
+	class Ray
+	{
+		private:
+
+		public:
+			Ray(mlib::vec4 pos, mlib::vec4 dir);
+	};
+
+}
+
+#endif
+
