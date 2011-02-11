cb3271c1764bf889746388c26fbc53af2a66f514
AoD314 <morozov.andrey.vmk@gmail.com>
2011-02-11 16:33:21 +0300

    Исправил ошибку с камерой

--- rt2/camera.cpp
+++ rt2/camera.cpp
@@ -3,6 +3,8 @@
 
 using namespace mlib;
 
+const float rad_to_angle = 3.1415926535897932384626433832795f / 180.0f;
+
 namespace rt2
 {
 	Camera::Camera()
@@ -14,7 +16,7 @@ namespace rt2
 		width  = 512;
 		height = 512;
 		aspect = static_cast<float>(width) / static_cast<float>(height);
-		tan_aview = tan (45.0f);
+                tan_aview = tan (60.0f * rad_to_angle);
 	}
 
 	Camera::Camera(vec4 p, vec4 d, vec4 du, unsigned int w, unsigned int h, float angleofview)
@@ -26,7 +28,7 @@ namespace rt2
 		width = w;
 		height = h;
 		aspect = static_cast<float>(width) / static_cast<float>(height);
-		tan_aview = tan (angleofview);
+                tan_aview = tan (angleofview * rad_to_angle);
 	}
 
 	Camera::Camera(const Camera & cam)
@@ -56,23 +58,19 @@ namespace rt2
 		float xx = 2.0f * (static_cast<float>(i) / static_cast<float>(width) ) - 1.0f;
 		float yy = 1.0f - 2.0f * (static_cast<float>(j) / static_cast<float>(height) );
 
-		float deltaX =   2.0f / width ;
-		float deltaY = - 2.0f / height;
-
 		if (allpart > 1)
 		{
-			deltaX /= static_cast<float>(allpart - 1);
-			deltaY /= static_cast<float>(allpart - 1);
+                        float deltaX =   2.0f / static_cast<float>(width  * (allpart - 1));
+                        float deltaY = - 2.0f / static_cast<float>(height * (allpart - 1));
 
-			xx += deltaX * (part % allpart);
-			yy += deltaY * (part / allpart);
+                        xx += deltaX * static_cast<float>(part % allpart);
+                        yy += deltaY * static_cast<float>(part / allpart);
 		}
 
 		xx *= (aspect * tan_aview);
 		yy *= tan_aview;
 
-		vec4 direction = dir + dir_lf * xx + dir_up * yy;
-		direction.normalize();
+                vec4 direction = normalize(dir + dir_lf * xx + dir_up * yy);
 
 		return Ray(pos, direction);
 	}
