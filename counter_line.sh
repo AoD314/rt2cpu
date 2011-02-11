175a24879b5b5ed6cb7166c33365f50c61b552f4
AoD314 <morozov.andrey.vmk@gmail.com>
2011-02-11 20:14:05 +0300

    Добавил информацию об антиалиасенге в help к программе. Добавил скрипт, который выводит статистику по коду.

---
+++ counter_line.sh
@@ -0,0 +1,77 @@
+
+list_mlib=`find ./mlib -type f -name '*.[c,h]pp'`
+list_rt2=`find ./rt2 -type f -name '*.[c,h]pp'`
+list_tests=`find ./tests -type f -name '*.[c,h]pp'`
+list_all=`find . -type f -name '*.[c,h]pp'`
+list_tex=`find . -type f -name '*.tex'`
+list_python=`find . -type f -name '*.py'`
+
+printf "\nmodule mlib:\n"
+printf "Lines:   Count byte:      File name:\n"
+
+for arg in $list_mlib 
+do
+cl=`cat $arg | wc -l`
+sl=`cat $arg | wc -c`
+name=`basename $arg`
+printf "%5d %13d %5s %-30s\n" $cl $sl " " "$name"
+done
+
+printf "\nmodule rt2:\n"
+printf "Lines:   Count byte:      File name:\n"
+
+for arg in $list_rt2
+do
+cl=`cat $arg | wc -l`
+sl=`cat $arg | wc -c`
+name=`basename $arg`
+printf "%5d %13d %5s %-30s\n" $cl $sl " " "$name"
+done
+
+printf "\nmodule tests:\n"
+printf "Lines:   Count byte:      File name:\n"
+
+for arg in $list_tests
+do
+cl=`cat $arg | wc -l`
+sl=`cat $arg | wc -c`
+name=`basename $arg`
+printf "%5d %13d %5s %-30s\n" $cl $sl " " "$name"
+done
+
+printf "\nmodule docs:\n"
+printf "Lines:   Count byte:      File name:\n"
+
+for arg in $list_tex
+do
+cl=`cat $arg | wc -l`
+sl=`cat $arg | wc -c`
+name=`basename $arg`
+printf "%5d %13d %5s %-30s\n" $cl $sl " " "$name"
+done
+
+printf "\nmodule python:\n"
+printf "Lines:   Count byte:      File name:\n"
+
+for arg in $list_python
+do
+cl=`cat $arg | wc -l`
+sl=`cat $arg | wc -c`
+name=`basename $arg`
+printf "%5d %13d %5s %-30s\n" $cl $sl " " "$name"
+done
+
+printf "\nAll:\n"
+all_l=`find . -type f -name '*.[c,h]pp' -exec cat {} \; | wc -l`
+all_t=`find . -type f -name '*.tex' -exec cat {} \; | wc -l`
+all_p=`find . -type f -name '*.py' -exec cat {} \; | wc -l`
+all_c=`find . -type f -name '*.[c,h]pp' -exec cat {} \; | wc -c`
+echo "Total lines source code : $all_l"
+echo "Total lines docs : $all_t"
+echo "Total lines python scripts : $all_p"
+kb=`echo $all_c/1024 | bc `
+echo "Total size source code  : $all_c byte = $kb KByte"
+total=`echo $all_l+$all_t+$all_p | bc`
+echo " "
+echo "TOTAL LINE WORK: $total"
+
--- rt2cpu/settings.cpp
+++ rt2cpu/settings.cpp
@@ -45,6 +45,7 @@ void Settings::print_params()
         std::cout << "Console Parameters:";
         std::cout << "\nResolution                     : " << width << "x" << height;
         std::cout << "\nCount threads                  : " << threads;
+        std::cout << "\nAntialiasing                   : " << antialiasing;
         std::cout << "\nDepth ray tracing              : " << depth;
         std::cout << "\nMax count objects in BVH tree  : " << max_count_objects_in_bvh;
         std::cout << "\nCount lights                   : " << lights;
@@ -64,19 +65,20 @@ void Settings::print_help()
         std::cout << "Real Time Ray Tracing on CPU";
         std::cout << "\n\nHelp:";
         std::cout << "\n\t--help, --usage       - Help informations (this text)";
-        std::cout << "\n\t--width    <int>(-w)  - Width image";
-        std::cout << "\n\t--height   <int>(-h)  - Height image";
-        std::cout << "\n\t--frame    <int>(-f)  - Count frame before exit";
-        std::cout << "\n\t--threads  <int>(-t)  - Count threads in OpenMP or TBB";
-        std::cout << "\n\t--depth    <int>(-d)  - Depth of ray tracing";
-        std::cout << "\n\t--bvhobj   <int>(-bo) - Max count objects in BVH tree";
-        std::cout << "\n\t--lights   <int>(-l)  - Count lights";
-        std::cout << "\n\t--obj      <str>(-o)  - Path to obj file (object for render)";
-        std::cout << "\n\t--print     (-p)      - Print parameters";
-        std::cout << "\n\t--benchmark (-b)      - Benchmark";
-        std::cout << "\n\t--quiet     (-q)      - No output";
-        std::cout << "\n\t--saveimg   (-s)      - Save image to file";
-        std::cout << "\n\t--event     (-e)      - Event";
+        std::cout << "\n\t--width        <int>(-w)  - Width image";
+        std::cout << "\n\t--height       <int>(-h)  - Height image";
+        std::cout << "\n\t--frame        <int>(-f)  - Count frame before exit";
+        std::cout << "\n\t--threads      <int>(-t)  - Count threads in OpenMP or TBB";
+        std::cout << "\n\t--antialiasing <int>(-a)  - Antialiasing";
+        std::cout << "\n\t--depth        <int>(-d)  - Depth of ray tracing";
+        std::cout << "\n\t--bvhobj       <int>(-bo) - Max count objects in BVH tree";
+        std::cout << "\n\t--lights       <int>(-l)  - Count lights";
+        std::cout << "\n\t--obj          <str>(-o)  - Path to obj file (object for render)";
+        std::cout << "\n\t--print        (-p)      - Print parameters";
+        std::cout << "\n\t--benchmark    (-b)      - Benchmark";
+        std::cout << "\n\t--quiet        (-q)      - No output";
+        std::cout << "\n\t--saveimg      (-s)      - Save image to file";
+        std::cout << "\n\t--event        (-e)      - Event";
         std::cout << std::endl;
 }
 
