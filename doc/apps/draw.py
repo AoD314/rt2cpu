7788e543804bddfc8df3f44938fcc0142fedda54
AoD314 <morozov.andrey.vmk@gmail.com>
2011-03-04 04:51:39 +0300

    Обновил скрипт, который строит графики и сохраняет их в eps файлы. Добавил немного документации про класс Timer.

--- doc/apps/draw.py
+++ doc/apps/draw.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from pylab import *
-import subprocess
-from os import *
+
+import sys, subprocess, os
+import matplotlib.pyplot as plt
+
+#from matplotlib import rcParams
+#rcParams['text.usetex']=True
+#rcParams['text.latex.unicode']=True
+
+from matplotlib import rc 
+rc('font',**{'family':'serif'}) 
+rc('text', usetex=True) 
+rc('text.latex',unicode=True) 
+rc('text.latex',preamble='\usepackage[utf8]{inputenc}') 
+rc('text.latex',preamble='\usepackage[russian]{babel}') 
+
+def progressbar(p):
+	print chr(27) + '[A' + "progress : " + str(p) + "%"
 
 def run(p,t):
-	s = "./a.out " + str(p) + " " + str(t)
-	output = str(popen(s, "r").read())
+	s = "./cmp_virtual_method " + str(p) + " " + str(t)
+	output = str(os.popen(s, "r").read())
 	return output[:-1]
 
 x = []
 y = []
-for i in range(1, 300):
-	print str(i) + " / 300"
+r = range(1,91)
+for i in r:
+	progressbar(i * 100 / len(r))
 	x = x + [i]
-	y = y + [int(run(i, 128))]
+	y = y + [int(run(i, 1024 * 1024))]
 
-plot(x,y)
-
-print "done."
-
-show()
-
-x = []
-y = []
-for i in range(1, 101):
-	print str(i) + " / 100"
-	x = x + [i*600]
-	y = y + [int(run(i*600, 8))]
-
-plot(x,y)
+plt.plot(x, y, aa=True)
+plt.ylabel(unicode('Накладные расходы (в \%)', 'utf-8'))
+plt.xlabel(unicode('Количесво объектов в массиве', 'utf-8'))
+plt.title (unicode('Накладные расходы при наследование', 'utf-8'))
+plt.grid(True)
+plt.savefig("compare_virtual_methods.eps")
 
 print "done."
-
-show()
-
--- doc/build_docs.py
+++ doc/build_docs.py
@@ -5,9 +5,9 @@ import os
 print "cpp -> tex"
 os.system('cd ./src/; python ./convert-cpp-to-tex.py')
 
-
+print "create compare"
+os.system('cd ./apps/; python ./draw.py')
 
 print "generate docs"
 os.system('texi2pdf rt2cpu.tex')
 os.system('texi2pdf rt2cpu.tex')
-
