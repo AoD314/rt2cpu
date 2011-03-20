dd29dca885333314c4abccf3d1e79403cf0a6ead
AoD314 <morozov.andrey.vmk@gmail.com>
2011-03-20 18:17:36 +0300

    Добавил результаты в главу про сглаживание.

--- doc/build_docs.py
+++ doc/build_docs.py
@@ -8,6 +8,10 @@ os.system('cd ..; scons -c; scons')
 print "cpp -> tex"
 os.system('cd ./src/; python ./convert-cpp-to-tex.py')
 
+print "create pattern"
+os.system('cd ./imgs/; python aa_example.py')
+os.system('cd ./imgs/; python aa_pattern.py')
+
 print "create compare"
 os.system('cd ./apps/; python ./cmp_virtual_method.py')
 
