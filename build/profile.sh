24ef9cf87a652f43e5b57cbe04cb9337b5321580
AoD314 <morozov.andrey.vmk@gmail.com>
2011-05-29 21:18:07 +0400

    Изменил параметры профилирования программ.

--- build/profile.sh
+++ build/profile.sh
@@ -1,2 +1,2 @@
-valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes --collect-bus=yes --simulate-cache=yes --L2=262144,8,64 --I1=32768,4,64 --D1=32768,8,64 --time-stamp=yes ./rt2cpu -f 1 -w 1024 -h 1024 -d 2 -l 2 -a 6
+valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes --collect-bus=yes --simulate-cache=yes --L2=262144,8,64 --I1=32768,4,64 --D1=32768,8,64 --time-stamp=yes ./rt2cpu -t 12 -w 512 -h 512 -a 3 -d 2 -f 1 -g 8192
 # --LL=12582912,16,64 --I1=32768,4,64 --D1=32768,8,64 
